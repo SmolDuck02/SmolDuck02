9f038bda77d175249351502124d9e43812b0b56b
Jameel Marco C. Ursonal <131737117+SmolDuck02@users.noreply.github.com>
2023-09-30 12:12:25 +0800

    Delete products directory

--- products/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class ProductCategory(models.Model):
-  product_category = models.CharField(max_length=255)
-
-
-class Users(models.Model):
-  username = models.CharField(max_length=255)
-  password = models.CharField(max_length=255)
